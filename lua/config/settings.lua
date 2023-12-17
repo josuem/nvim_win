0afaaa0fba1edf02c515f0ef3a109b8f3b1a6a5a
josuem <josue.meneses@usach.cl>
2023-12-17 14:17:47 -0800

    Added gitsigns to configuration in git.lua

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -6,3 +6,4 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
+vim.g.mapleader = ' '
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,9 +1,20 @@
 return {
-	"tpope/vim-fugitive",
-	cmd = {"G", "Git"},
-	keys = {
-		{"<leader>gc", ":Git commit", desc = "Git commit"},
-		{"<leader>gp", ":Git pull", desc = "Git pull"},
-		{"<leader>gs", ":Git push", desc = "Git push"},
-	}
+	{
+		"tpope/vim-fugitive",
+		cmd = {"G", "Git"},
+		keys = {
+			{"<leader>gc", ":Git commit", desc = "Git commit"},
+			{"<leader>gp", ":Git pull", desc = "Git pull"},
+			{"<leader>gs", ":Git push", desc = "Git push"},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = true,
+		opts = {
+			-- signcolumn = false,
+			numhl = true,
+			max_file_length = 10000,
+		}
+	},
 }
