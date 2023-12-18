48b456dab80c9c98a18006e1b4f4f9eb04e0b1d5
josuem <josue.meneses@usach.cl>
2023-12-18 09:50:57 -0800

    Added configuration for autocomplete and snippets

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,7 @@
+-- Using configuration from https://github.com/the-alpha-developer/mastering-neovim/blob/main/lua/plugins/luasnip.lua
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  main = "config.plugins.luasnip",
+  config = true,
+}
